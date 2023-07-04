0920951576d0053d4c1e9eb7c70650fb2a09654a
sharath <sharath.surendran.m@gmail.com>
2023-07-04 19:09:42 +0530

    session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,20 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.session.isLoggedIn;
   console.log(isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
   req.session.isLoggedIn = true;
   res.redirect('/');
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
