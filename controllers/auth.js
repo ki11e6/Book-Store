4f9036452a158168f4e5b3ddc8259f666400981a
sharath <sharath.surendran.m@gmail.com>
2023-07-08 18:07:38 +0530

    refactor

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    type: 'OAuth2',
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-  },
-});
+const sendEmail = require('../util/nodemailGmail');
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -99,16 +87,13 @@ exports.postSignup = (req, res, next) => {
         .then((result) => {
           res.redirect('/login');
           //signup success mail
-          let mailOptions = {
-            from: process.env.FROM_EMAIL,
-            to: email,
-            subject: 'Signup Success',
-            html: '<h1>Dear user, Your have Successfully Signed in to BookStore!</h1',
-          };
-          return transporter
-            .sendMail(mailOptions)
-            .then(() => console.log('mail sent successfully'))
-            .catch((error) => console.log('Error:' + error));
+          return sendEmail(email, (err) => {
+            if (err) {
+              console.log('Error:' + err);
+            } else {
+              console.log('mail sent successfully');
+            }
+          });
         });
     })
     .catch((err) => {
--- util/nodemailGmail.js
+++ util/nodemailGmail.js
@@ -1,26 +1,33 @@
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    type: 'OAuth2',
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-  },
-});
+const nodemailer = require('nodemailer');
 
-let mailOptions = {
-  from: process.env.FROM_EMAIL,
-  to: email,
-  subject: 'Nodemailer Project',
-  text: 'Hi from your nodemailer project',
+const sentEmail = (email, callback) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.FROM_EMAIL,
+    to: email,
+    subject: 'Signup Success',
+    text: 'Welcome to BookStore',
+    html: '<h2>Your have Successfully Signed in to BookStore!<h2>',
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
+    }
+  });
 };
 
-transporter.sendMail(mailOptions, function (err, data) {
-  if (err) {
-    console.log('Error ' + err);
-  } else {
-    console.log('Email sent successfully');
-  }
-});
+module.exports = sentEmail;
