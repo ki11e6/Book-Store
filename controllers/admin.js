8c71da7afe914bb71e411b4ed4cdb8b3e58e9bd2
sharath <sharath.surendran.m@gmail.com>
2023-07-13 14:25:29 +0530

    delete prods also delete file

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 const { validationResult } = require('express-validator');
+const fileHelper = require('../util/file-helper');
 
 exports.getAddProduct = (req, res) => {
   res.render('admin/edit-product', {
@@ -135,6 +136,7 @@ exports.postEditProduct = (req, res, next) => {
       product.price = updatedPrice;
       product.description = updatedDesc;
       if (image) {
+        fileHelper.deleteFile(product.imageUrl);
         product.imageUrl = image.path;
       }
       return product.save().then(() => {
@@ -170,10 +172,18 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteOne({ _id: prodId, userId: req.user._id })
-    .then(() => {
-      console.log('DESTROYED PRODUCT');
-      res.redirect('/admin/products');
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return next(new Error('Product not found'));
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id }).then(
+        () => {
+          console.log('DESTROYED PRODUCT');
+          res.redirect('/admin/products');
+        }
+      );
     })
     .catch((err) => {
       const error = new Error(err);
